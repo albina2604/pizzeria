2601191086f0d29f29b66a41ccaa78c56417775c
albina2604 <albinamilshyna2604@gmail.com>
2023-03-11 16:31:04 +0200

    Add pizza_type page

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Сторінка з переліком усіх тем
     path('pizza_types/', views.pizza_types, name='pizza_types'),
+    # Сторінка з складниками до піци
+    path('pizza_type/<int:pizza_type_id>/',
+         views.pizza_type, name='pizza_type'),
+
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -14,3 +14,11 @@ def pizza_types(request):
     pizza_types = Pizza.objects.order_by('date_added')
     context = {'pizza_types': pizza_types}
     return render(request, 'pizzas/pizza_types.html', context)
+
+
+def pizza_type(request, pizza_type_id):
+    """Виводить одну тему і всі ії записи"""
+    pizza_type = Pizza.objects.get(id=pizza_type_id)
+    toppings = pizza_type.topping_set.order_by('-date_added')
+    context = {'pizza_type': pizza_type, 'toppings': toppings}
+    return render(request, 'pizzas/pizza_type.html', context)
